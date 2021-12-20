6dca8b261fba518d98a3b236fb064de5119f844b
Manh Xng <nguyenxuanmanh17112000@gmail.com>
2021-12-20 10:54:06 +0700

    first commit

---
+++ BaiTap10/Controllers/ProductsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BaiTap10.Models;
+
+namespace BaiTap10.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private WineStoreDB db = new WineStoreDB();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Catalogy);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CatalogyID = new SelectList(db.Catalogies, "CatalogyID", "CatalogyName");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,Description,PurchasePrice,Price,Quantity,Vintage,CatalogyID,Image,Region")] Product product)
+        {
+            try
+                {
+                if (ModelState.IsValid)
+                    {
+                    product.Image = "";
+                    var f = Request.Files["ImageFile"];
+                    if (f !=null && f.ContentLength > 0)
+                    {
+                        // user Namespace  called: System.IO
+                        string FileName = System.IO.Path.GetFileName(f.FileName);
+                        // Lấy tên file Upload
+                        string UploadPath = Server.MapPath("~/wwwroot/WineImages/" + FileName);
+                        // Copy and save file vào server
+                        f.SaveAs(UploadPath);
+                        // Lưu tên file vào trường Image
+                        product.Image = FileName;
+                    }
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }catch(Exception ex)
+            {
+                ViewBag.Error = "Lỗi nhập dữ liệu!" + ex.Message;
+                return View(product);
+            }
+ 
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CatalogyID = new SelectList(db.Catalogies, "CatalogyID", "CatalogyName", product.CatalogyID);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,Description,PurchasePrice,Price,Quantity,Vintage,CatalogyID,Image,Region")] Product product)
+        {
+            try {
+                if (ModelState.IsValid)
+                {
+                    product.Image = "";
+                    var f = Request.Files["ImageFile"];
+                    if (f != null && f.ContentLength > 0)
+                    {
+                        // user Namespace  called: System.IO
+                        string FileName = System.IO.Path.GetFileName(f.FileName);
+                        // Lấy tên file Upload
+                        string UploadPath = Server.MapPath("~/wwwroot/WineImages/" + FileName);
+                        // Copy and save file vào server
+                        f.SaveAs(UploadPath);
+                        // Lưu tên file vào trường Image
+                        product.Image = FileName;
+                    }
+                    db.Entry(product).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Error = "Lỗi sửa dữ liệu" + ex.Message;
+                return View(product);
+            }
+    }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
